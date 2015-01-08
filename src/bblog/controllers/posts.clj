67b05a18d5a5a43d01993bf721de401acc6dac6d
bbarm <minsoo1003@gmail.com>
2015-01-08 16:13:31 +0900

    clostache dependency added, posts index page template added

--- src/bblog/controllers/posts.clj
+++ src/bblog/controllers/posts.clj
@@ -1,3 +1,13 @@
-(ns bblog.controllers.posts)
+(ns bblog.controllers.posts
+  (:require
+    [clostache.parser :as clostache]))
 
-(defn index [] "hello, from index")
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "views/posts/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+(defn index []
+  (render-template "index" {:name "BBarm"}))
